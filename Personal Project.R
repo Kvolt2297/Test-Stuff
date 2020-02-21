b982b6038d6647cabfa496f9e6ae4bb5bf76cc7c
Kvolt2297 <voldoko_kirill@users.noreply.github.com>
2020-02-21 02:22:28 -0800

    Small Edits

--- Personal Project.R
+++ Personal Project.R
@@ -15,31 +15,28 @@
 #Tables = No stress/Neutral, No stress/disparaging, stress/neutral
 #stress/disparaging
 
-#Placeholder group to put in values in the future
-experiment <- c(NA)
-
 #Making the groups. 25 participants for each (100 in total).
 total.participants <- 100  #total amount of participants in the experiment
-
-
-
 n.participants <- 25 #how any participants in each group
 
-#Assigning the participants to each of the group
-part.stress.neutral <- n.participants
-part.stress.disparaging <- n.participants 
-part.stress.free.neutral <- n.participants 
-part.stress.free.disparaging <- n.participants 
+#My attempts at manually inputting participant number into specific groups and 
+#then placing binding them into rows... it doesn't work like that 
 
-testing <- data.frame(c(1:25),stress.TSST.neutral, stress.TSST.disparaging)
-
-row(part.stress.neutral)
-
-testing <- rbind (part.stress.neutral, part.stress.free.disparaging, 
-      part.stress.free.neutral, part.stress.free.disparaging)
-
-
-      cbind(stress.TSST.neutral, stress.TSST.disparaging)
+#Assigning the participants to each of the group 
+# part.stress.neutral <- n.participants
+# part.stress.disparaging <- n.participants 
+# part.stress.free.neutral <- n.participants 
+# part.stress.free.disparaging <- n.participants 
+# 
+# testing <- data.frame(c(1:25),stress.TSST.neutral, stress.TSST.disparaging)
+# 
+# row(part.stress.neutral)
+# 
+# testing <- rbind (part.stress.neutral, part.stress.free.disparaging, 
+#       part.stress.free.neutral, part.stress.free.disparaging)
+# 
+# 
+#       cbind(stress.TSST.neutral, stress.TSST.disparaging)
 
 
 
@@ -51,7 +48,6 @@ stress.level <- function(x, y) {
 round(runif((n.participants), min = x, max = y), 0)  #runif selects random 
   #numbers from a certain range
 }
-
 #Testing the function
 stress.level(1, 10)
 
@@ -59,6 +55,7 @@ stress.level(1, 10)
 
 #storage <- vector("numeric", 100) < Attempted to make a storage area with 
 #100 empty spaces and fill it with NA
+
 names.groups <- c("Stressed Neutral", "Stress Disparaging", "No Stress Neutral", 
                   "No Stress Disparaging")
 #Storage for 4 groupsof n.participants. Fill it with NA's
@@ -67,8 +64,20 @@ storage <- cbind.data.frame(group = rep(names.groups, rep(n.participants,4)),
                  middle = rep(NA, 4*n.participants),
                  finish = rep(NA, 4*n.participants)
                  )
+head(storage) #Check
 
-head(storage)
+#This was me trying to assign every group in every variable for a stress levels
+#Farmers way
+stress.TSST.neutral <- stress.level(4, 8)
+stress.TSST.disparaging <- stress.level(4, 8)
+
+#Assigning stress level after hearing jokes in four different groups
+stress.after.neutral <- stress.level(3, 7)
+stress.after.disparaging <- stress.level(2, 6)
+
+stress.free.after.neutral <- stress.level(1,3)
+stress.free.after.disparaging <- stress.level(1,3)
+
 for(i in 1:4){
   #Enter for loop
   # i <- 1 #Reset loop
@@ -110,7 +119,6 @@ if (i == 4) { #First group - No Stress Disparaging
 }
 } #Provides me with stress levels for each row and column!  
 
- 
 #tried to use name indexing to make R look at the name of the row and give me 
 #collumn values from that row. But since the name repeats itself 25 times, 
 # I get incorrect number of dimensions error
@@ -118,17 +126,5 @@ if (i == 4) { #First group - No Stress Disparaging
 #storage$start["Stressed neutral", ]
 
 
-start.stop
-#After the TSST test (stress condition only, which is half of them)
 
-#Farmers way
-stress.TSST.neutral <- stress.level(4, 8)
-stress.TSST.disparaging <- stress.level(4, 8)
-
-#Assigning stress level after hearing jokes in four different groups
-stress.after.neutral <- stress.level(3, 7)
-stress.after.disparaging <- stress.level(2, 6)
-
-stress.free.after.neutral <- stress.level(1,3)
-stress.free.after.disparaging <- stress.level(1,3)
 
