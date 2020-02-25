1a6f52bd57a0a8dffed7095cbb9bd02fc961b52d
Kvolt2297 <voldoko_kirill@users.noreply.github.com>
2020-02-25 09:21:23 -0800

    Seperating Code into Files. Fixing ANOVA file.
    Need to make a loop in ANOVA file that will make the whole thing work with any amount of participants

---
+++ Personal Project/Data Emulation.R
@@ -0,0 +1,45 @@
+#
+#
+# Volodko Kirill
+#
+#
+
+#Data Emulation------------------
+
+#Creating an empty storage
+a.storage <- cbind.data.frame(group = rep(names.groups, rep(n.participants,4)), 
+                              start = rep(NA, 4*n.participants), 
+                              middle = rep(NA, 4*n.participants),
+                              finish = rep(NA, 4*n.participants)
+)
+
+#
+#Emulating data through rnorm---------------------------------------------------                                                    
+i < 1
+for(i in 1:4){
+  # enter for loop
+  
+  start.stop <- c( ((i-1) * n.participants + 1), i * n.participants)
+  # First phase for every group has all the similar data.
+  a.storage$start[start.stop[1]:start.stop[2]] <- round(rnorm(n.participants, 
+                                        s.descr.table$Mean[i],
+                                        s.descr.table$Standard.Deviation[i]),0)                            
+  #Secobd phase only for the stressed groups
+  while(i <= 2) { #I am getting Not a Number (NaN) on my 76-100 rows...
+    a.storage$middle[start.stop[1]:start.stop[2]] <- round(rnorm(n.participants, 
+                                        m.descr.table$Mean[i],
+                                        m.descr.table$Standard.Deviation[i]),0)
+    break }
+  
+  #Third phase is for all, but every group has diffirent values
+  a.storage$finish[start.stop[1]:start.stop[2]] <- round(rnorm(n.participants, 
+                                        f.descr.table$Mean[i],
+                                        f.descr.table$Standard.Deviation[i]),0)
+}
+#Exporting rnorm data-------------------------------
+
+#Normal Distribution Data
+write.table(a.storage, 
+            file = paste(working.dir, "data/Normal Distribution.csv", 
+                         sep = "/"),
+            row.names=FALSE, sep = ",")
